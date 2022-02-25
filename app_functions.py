8214777316b49ce3a6e1d149815df2941fa6f24a
sharadbhandari <sharad.bhandari@gmail.com>
2022-02-25 23:31:49 +0545

    added horizontal scrollbar

--- app_functions.py
+++ app_functions.py
@@ -9,106 +9,160 @@ from tkinter import filedialog
 from app_banner import *
 import subprocess
 
+
 def cmd(arg):
     shell_output = subprocess.run(arg, stdout=subprocess.PIPE, shell=True)
     return shell_output
 
+
 def do_nothing():
     pass
+
 
 def start_ipfs(parent):
     cmd('ipfs init &')
     if cmd('pidof ipfs').returncode != 0:
         cmd('ipfs daemon > ipfs_start.log &')
 
-    parent.ipfs_status.config(text="IPFS daemon status: Running\nPID: " \
+    parent.ipfs_status.config(text="IPFS daemon status: Running\nPID: "
                                    + str(cmd('pidof ipfs').stdout.decode())[0:-1],
                               fg="#1e961c",
                               )
     parent.ipfs_status.config()
-    
+
     parent.button_start.config(state="disable")
     parent.button_stop.config(state="active")
-        
+
+
 def stop_ipfs(parent):
     pid_ipfs = cmd('pidof ipfs').stdout.decode()
     cmd('kill `pidof ipfs`')
-    
-    parent.ipfs_status.config(text="IPFS daemon status: Stopped\n PID: " \
-                                   + pid_ipfs[0:-1], 
+
+    parent.ipfs_status.config(text="IPFS daemon status: Stopped\n PID: "
+                                   + pid_ipfs[0:-1],
                               fg="#FF0000",
                               )
-    
+
     parent.button_start.config(state="active")
     parent.button_stop.config(state="disable")
-    
+
+
 def reconsruct_right_frame(parent):
     parent.frame_right.destroy()
-        
-    parent.frame_right = LabelFrame(parent.main_frame, 
-                                     text="Interface", 
-                                     padx=10, 
-                                     pady=10, 
-                                     relief="solid",
-                                     width=parent.width*0.70, 
-                                     height=parent.height*0.65
-                                     )
+
+    parent.frame_right = LabelFrame(parent.main_frame,
+                                    text="Interface",
+                                    padx=10,
+                                    pady=5,
+                                    relief="solid",
+                                    width=parent.width*0.70,
+                                    height=parent.height*0.65
+                                    )
     parent.frame_right.grid(row=0, column=1)
     parent.frame_right.grid_propagate(False)
 
+
 def add_files(parent):
     reconsruct_right_frame(parent)
-    
-    filename = filedialog.askopenfilename(initialdir = "./",
-                                          title = "Select a File",
-                                          filetypes = (("All files", "*"),)
+
+    filename = filedialog.askopenfilename(initialdir="./",
+                                          title="Select a File",
+                                          filetypes=(("All files", "*"),)
                                           )
-    
+
     if filename:
-        ipfs_add = cmd(f'ipfs add {filename}').stdout.decode()
+        ipfs_added = cmd(f'ipfs add {filename}').stdout.decode()
+
+        with open("ipfs_added_list.txt", "a") as f:
+            f.write(filename.split("/")[-1] +
+                    "  >  " + ipfs_added.split()[1] + "\n")
+
+        # ipfs_added = f"{filename} added to IPFS\n\n {ipfs_added.split()[1]}"
+
+        Label(parent.frame_right,
+              foreground="#1e961c",
+              text="File added to IPFS",
+              bd=2,
+              relief="ridge",
+              pady=5,
+              ).grid(row=0, column=0, sticky="news")
         
-        with open ("ipfs_added_list.txt", "a") as f:
-            f.write(filename.split("/")[-1] + "  >  " + ipfs_add.split()[1] + "\n")
-            
-        ipfs_add = f"{filename} added to IPFS\n\n {ipfs_add.split()[1]}"
+        Label(parent.frame_right,
+              text=filename,
+              bd=2,
+              relief="ridge",
+              pady=5,
+              ).grid(row=1, column=0, sticky="news")
         
-        parent.main_label = Label(parent.frame_right,
-                             text=ipfs_add,
-                             relief="solid",
-                             pady=5,
-                             )
-        parent.main_label.grid(row=0, column=0, sticky="w")
-    
+        Label(parent.frame_right,
+              text="",
+              ).grid(row=2, column=0, sticky="news")
+        
+        Label(parent.frame_right,
+              foreground="#1e961c",
+              text="Hash",
+              bd=2,
+              relief="ridge",
+              pady=5,
+              ).grid(row=3, column=0, sticky="news")
+        
+        Label(parent.frame_right,
+              text=ipfs_added.split()[1],
+              bd=2,
+              relief="ridge",
+              pady=5,
+              ).grid(row=4, column=0, sticky="news")
+
+
 def view_files(parent):
     reconsruct_right_frame(parent)
 
-    parent.canvas_frame = LabelFrame(parent.frame_right, bd=1, relief="solid")
-    parent.canvas_frame.grid(row=0, column=0)
-        
-    parent.canvas = Canvas(parent.canvas_frame, width=500)
-    parent.canvas.grid(row=0, column=0, sticky="ws")
+    parent.canvas_frame = LabelFrame(parent.frame_right,
+                                     bd=2,
+                                     relief="sunken",
+                                     width=parent.width*.65,
+                                     height=parent.height*0.55,
+                                     )
+    parent.canvas_frame.grid(row=0, column=0,)
 
-    parent.y_scroll = ttk.Scrollbar(parent.canvas_frame, orient=VERTICAL, command=parent.canvas.yview)
+    parent.canvas = Canvas(parent.canvas_frame,
+                           width=parent.width*.65,
+                           height=parent.height*0.55,
+                           )
+    parent.canvas.grid(row=0, column=0,)
+
+    parent.x_scroll = ttk.Scrollbar(parent.canvas_frame,
+                                    orient=HORIZONTAL,
+                                    command=parent.canvas.xview)
+    parent.x_scroll.grid(row=1, column=0, sticky="nwse")
+
+    parent.y_scroll = ttk.Scrollbar(parent.canvas_frame,
+                                    orient=VERTICAL,
+                                    command=parent.canvas.yview)
     parent.y_scroll.grid(row=0, column=1, sticky="nwse")
 
-    parent.canvas.configure(yscrollcommand=parent.y_scroll.set)
-    parent.canvas.bind('<Configure>', lambda e: parent.canvas.configure(scrollregion=parent.canvas.bbox("all")))
+    parent.canvas.configure(xscrollcommand=parent.x_scroll.set,
+                            yscrollcommand=parent.y_scroll.set,
+                            )
+    parent.canvas.bind('<Configure>', lambda e: parent.canvas.configure(
+        scrollregion=parent.canvas.bbox("all")))
 
     parent.canvas_frame_in = Frame(parent.canvas)
 
-    parent.canvas.create_window((0,0), window=parent.canvas_frame_in, anchor="nw")
-    
-    with open ("ipfs_added_list.txt", "r") as f:
-        line_count=0
+    parent.canvas.create_window(
+        (0, 0), window=parent.canvas_frame_in, anchor="nw")
+
+    with open("ipfs_added_list.txt", "r") as f:
+        line_count = 0
         for line in f:
             parent.main_label = Label(parent.canvas_frame_in,
-                                 text=line[:-1],
-                                 relief="solid",
-                                 pady=5,
-                                 )
+                                      text=line[:-1],
+                                      relief="solid",
+                                      pady=5,
+                                      )
             parent.main_label.grid(row=line_count, column=0, sticky="w")
             # qr_buttons(parent, line_count)
-            line_count+=1
+            line_count += 1
 
 # def qr_buttons(parent, count):
 #     parent.button_view_files = Button(parent.frame_right_in,
--- app_gui.py
+++ app_gui.py
@@ -1,10 +1,12 @@
 from app_functions import *
 from app_banner import *
 
+
 def main():
     root = Tk()
     main_gui(root, 900, 600, "IPFS file exchange")
     root.mainloop()
+
 
 class main_gui:
     def __init__(self, parent, width, height, title):
@@ -13,113 +15,115 @@ class main_gui:
         self.height = height
         self.parent.geometry(f"{str(self.width)}x{str(self.height)}")
         self.parent.title("IPFS file share")
-        
+
         self.main_frame = Frame(self.parent)
-        self.main_frame.grid(padx=self.width*0.022, 
+        self.main_frame.grid(padx=self.width*0.022,
                              pady=self.height*0.05,
                              )
-        
-        self.frame_left = LabelFrame(self.main_frame, 
-                                        text="Menu",
-                                        relief="solid",
-                                        padx=10,
-                                        width=self.width*0.20, 
-                                        height=self.height*0.9,
-                                        )
+
+        self.frame_left = LabelFrame(self.main_frame,
+                                     text="Menu",
+                                     relief="solid",
+                                     padx=10,
+                                     width=self.width*0.20,
+                                     height=self.height*0.9,
+                                     )
         self.frame_left.grid(row=0, column=0, rowspan=2, padx=15)
-        self.frame_left.grid_propagate(False) 
-        
+        self.frame_left.grid_propagate(False)
+
         self.button_add_files = Button(self.frame_left,
-                                          text="Add file",
-                                          command=lambda:add_files(self),
-                                          borderwidth=1,
-                                          width=int(self.width*0.02),
-                                          relief="solid",
-                                          )
+                                       text="Add file",
+                                       command=lambda: add_files(self),
+                                       borderwidth=1,
+                                       width=int(self.width*0.02),
+                                       relief="solid",
+                                       )
         self.button_add_files.grid(row=0, column=0, )
-        
+
         self.button_view_files = Button(self.frame_left,
-                                           text="View files",
-                                           command=lambda:view_files(self),
-                                           borderwidth=1,
-                                           width=int(self.width*0.02),
-                                           relief="solid",
-                                           )
+                                        text="View files",
+                                        command=lambda: view_files(self),
+                                        borderwidth=1,
+                                        width=int(self.width*0.02),
+                                        relief="solid",
+                                        )
         self.button_view_files.grid(row=1, column=0)
-        
-        self.frame_right = LabelFrame(self.main_frame, 
-                                         text="Interface", 
-                                         padx=10, 
-                                         pady=10, 
-                                         relief="solid",
-                                         width=self.width*0.70, 
-                                         height=self.height*0.65
-                                         )
+
+        self.frame_right = LabelFrame(self.main_frame,
+                                      text="Interface",
+                                      relief="solid",
+                                      width=self.width*0.70,
+                                      height=self.height*0.65,
+                                      )
         self.frame_right.grid(row=0, column=1)
         self.frame_right.grid_propagate(False)
-        
-        self.frame_right_in = Frame(self.frame_right, 
+
+        self.frame_right_in = Frame(self.frame_right,
                                     relief="solid",
                                     )
-        self.frame_right_in.grid()
-        
+        self.frame_right_in.grid(row=0, column=0)
+
         self.banner = Label(self.frame_right_in,
-                               text=banner,
-                               padx=125, 
-                               pady=50,
-                               justify="center",
-                               )
+                            text=banner,
+                            padx=125,
+                            pady=50,
+                            justify="center",
+                            )
         self.banner.grid()
 
-        self.frame_bottom = LabelFrame(self.main_frame, 
-                                                text="IPFS status",
-                                                padx=20,
-                                                pady=5,
-                                                relief="solid",
-                                                width=self.width*0.70, 
-                                                height=self.height*0.25)
+        self.frame_bottom = LabelFrame(self.main_frame,
+                                       text="IPFS status",
+                                       padx=20,
+                                       pady=5,
+                                       relief="solid",
+                                       width=self.width*0.70,
+                                       height=self.height*0.25,
+                                       )
         self.frame_bottom.grid(row=1, column=1)
         self.frame_bottom.grid_propagate(False)
-        
+
         self.button_start = Button(self.frame_bottom,
-                                      text="Start IPFS",
-                                      command=lambda:start_ipfs(self),
-                                      borderwidth=2,
-                                      width=int(self.width*0.023),
-                                      relief="solid",
-                                      state="disable" if (cmd('pidof ipfs').returncode == 0) else "active"
-                                      )
+                                   text="Start IPFS",
+                                   command=lambda: start_ipfs(self),
+                                   borderwidth=2,
+                                   width=int(self.width*0.023),
+                                   relief="solid",
+                                   state="disable" if (
+                                       cmd('pidof ipfs').returncode == 0) else "active",
+                                   )
         self.button_start.grid(row=0, column=0)
-        
+
         self.button_stop = Button(self.frame_bottom,
-                                     text="Stop IPFS",
-                                     command=lambda:stop_ipfs(self),
-                                     borderwidth=2,
-                                     width=int(self.width*0.023),
-                                     relief="solid",
-                                     state="disable" if (cmd('pidof ipfs').returncode == 1) else "active"
-                                     )
+                                  text="Stop IPFS",
+                                  command=lambda: stop_ipfs(self),
+                                  borderwidth=2,
+                                  width=int(self.width*0.023),
+                                  relief="solid",
+                                  state="disable" if (
+                                      cmd('pidof ipfs').returncode == 1) else "active"
+                                  )
         self.button_stop.grid(row=1, column=0)
-        
+
         if cmd('pidof ipfs').returncode == 0:
             self.ipfs_status = Label(self.frame_bottom,
-                                        text="IPFS daemon status: Running\nPID: " \
-                                            + str(cmd('pidof ipfs').stdout.decode())[0:-1],
-                                        borderwidth=6,
-                                        padx=25,
-                                        relief="ridge",
-                                        fg="#1e961c"
-                                        )
+                                     text="IPFS daemon status: Running\nPID: "
+                                     + str(cmd('pidof ipfs').stdout.decode())[0:-1],
+                                     borderwidth=6,
+                                     padx=25,
+                                     relief="ridge",
+                                     fg="#1e961c"
+                                     )
         else:
             self.ipfs_status = Label(self.frame_bottom,
-                                        text="IPFS daemon status: Stopped",
-                                        borderwidth=6,
-                                        padx=25,
-                                        relief="ridge",
-                                        fg="#FF0000",
-                                        )
+                                     text="IPFS daemon status: Stopped",
+                                     borderwidth=6,
+                                     padx=25,
+                                     relief="ridge",
+                                     fg="#FF0000",
+                                     )
 
         self.ipfs_status.grid(row=0, column=1, rowspan=2, padx=20)
+
 
 if __name__ == "__main__":
     main()
