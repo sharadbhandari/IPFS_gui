00f2c9fa2f564653324d288a7ef3470a4dbf7d25
sharadbhandari <sharad.bhandari@gmail.com>
2022-02-26 21:50:29 +0545

    added exit button

--- app_gui.py
+++ app_gui.py
@@ -5,7 +5,7 @@ from app_banner import *
 def main():
     root = Tk()
     main_gui(root, 900, 600, "IPFS file exchange")
-    
+
     root.mainloop()
 
 
@@ -27,7 +27,7 @@ class main_gui:
                                      relief="solid",
                                      padx=10,
                                      width=self.width*0.20,
-                                     height=self.height*0.9,
+                                     height=self.height*0.81,
                                      )
         self.frame_left.grid(row=0, column=0, rowspan=2, padx=15)
         self.frame_left.grid_propagate(False)
@@ -39,7 +39,16 @@ class main_gui:
                                        width=int(self.width*0.02),
                                        relief="solid",
                                        )
-        self.button_add_files.grid(row=0, column=0, )
+        self.button_add_files.grid(row=0, column=0)
+        
+        self.button_get_files = Button(self.frame_left,
+                                       text="Get file",
+                                       command=do_nothing,
+                                       borderwidth=1,
+                                       width=int(self.width*0.02),
+                                       relief="solid",
+                                       )
+        self.button_get_files.grid(row=1, column=0)
 
         self.button_view_files = Button(self.frame_left,
                                         text="View files",
@@ -48,7 +57,7 @@ class main_gui:
                                         width=int(self.width*0.02),
                                         relief="solid",
                                         )
-        self.button_view_files.grid(row=1, column=0)
+        self.button_view_files.grid(row=2, column=0)
 
         self.frame_right = LabelFrame(self.main_frame,
                                       text="Interface",
@@ -78,7 +87,7 @@ class main_gui:
                                        pady=5,
                                        relief="solid",
                                        width=self.width*0.70,
-                                       height=self.height*0.25,
+                                       height=self.height*0.16,
                                        )
         self.frame_bottom.grid(row=1, column=1)
         self.frame_bottom.grid_propagate(False)
@@ -124,6 +133,15 @@ class main_gui:
                                      )
 
         self.ipfs_status.grid(row=0, column=1, rowspan=2, padx=20)
+        
+        self.button_view_files = Button(self.main_frame,
+                                        text="Exit App",
+                                        command=exit,
+                                        borderwidth=2,
+                                        width=int(self.width*0.02),
+                                        relief="solid",
+                                        )
+        self.button_view_files.grid(row= 2, column = 1, sticky="e", pady=8)
 
 
 if __name__ == "__main__":
--- popup.py
+++ popup.py
@@ -2,65 +2,80 @@ from tkinter import *
 import qrcode
 from PIL import ImageTk, Image
 
+
 def qr_img(parent, data):
-    qr = qrcode.QRCode(version=1,
-                       error_correction=qrcode.constants.ERROR_CORRECT_L,
-                       box_size=10,
-                       border=4,
-                       )
-    qr.add_data(data)
-    qr.make(fit=True)
+      qr = qrcode.QRCode(version=1,
+                        error_correction=qrcode.constants.ERROR_CORRECT_L,
+                        box_size=10,
+                        border=4,
+                        )
+      qr.add_data(data)
+      qr.make(fit=True)
 
-    img = qr.make_image(fill_color="black", back_color="white")
+      img = qr.make_image(fill_color="black", back_color="white")
 
-    img.save("qr_image.png")
-    
+      img.save("qr_image.png")
+
+
 def popup(parent, data):
-    top = Toplevel()
+      parent.top = Toplevel()
 
-    frm = Frame(top, bd=2, height=200, width=200, relief="solid")
-    frm.grid()
-    frm.grid_propagate(True)
+      frm = Frame(parent.top, bd=2, height=200, width=200, relief="solid")
+      frm.grid()
+      frm.grid_propagate(True)
 
-    Label(frm,
-          foreground="#1e961c",
-          text="Filename",
-          bd=1,
-          relief="solid",
-        #   width=27,
-          ).grid(row=0, column=0, sticky="news")
+      Label(frm,
+            foreground="#1e961c",
+            text="Filename",
+            bd=1,
+            relief="solid",
+            #   width=27,
+            ).grid(row=0, column=0, sticky="news")
 
-    Label(frm,
-          text="filename will go here",
-          bd=1,
-          relief="solid",
-          ).grid(row=1, column=0, sticky="news")
+      Label(frm,
+            text="filename will go here",
+            bd=1,
+            relief="solid",
+            ).grid(row=1, column=0, sticky="news")
 
-    Label(frm,
-          text="",
-          ).grid(row=2, column=0, sticky="news")
+      Label(frm,
+            text="",
+            ).grid(row=2, column=0, sticky="news")
 
-    Label(frm,
-          foreground="#1e961c",
-          text="Hash",
-          bd=1,
-          relief="solid",
-          ).grid(row=3, column=0, sticky="news")
+      Label(frm,
+            foreground="#1e961c",
+            text="Hash",
+            bd=1,
+            relief="solid",
+            ).grid(row=3, column=0, sticky="news")
 
-    Label(frm,
-          text="hash will go here",
-          bd=1,
-          relief="solid",
-          ).grid(row=4, column=0, sticky="news")
-    
-    qr_img(parent, data)
-    
-    parent.img = Image.open("qr_image.png")
-    parent.img = ImageTk.PhotoImage(parent.img.resize((196, 196), Image.ANTIALIAS))
-    
-    Label(frm,
-          text="hash will go here",
-          image= parent.img,
-          bd=1,
-          relief="solid",
-          ).grid(row=5, column=0, sticky="news")
+      Label(frm,
+            text="hash will go here",
+            bd=1,
+            relief="solid",
+            ).grid(row=4, column=0, sticky="news")
+
+      qr_img(parent, data)
+
+      parent.img = Image.open("qr_image.png")
+      parent.img = ImageTk.PhotoImage(
+            parent.img.resize((196, 196), Image.ANTIALIAS))
+
+      Label(frm,
+            text="hash will go here",
+            image=parent.img,
+            bd=1,
+            relief="solid",
+            ).grid(row=5, column=0, sticky="news")
+
+      Button(frm,
+            text='EXIT',
+            command=lambda: top_exit(parent),
+            bd=1,
+            foreground="#FF0000",
+            relief="solid",
+            ).grid(row=6, column=0, sticky="news")
+
+
+def top_exit(parent):
+      parent.top.destroy()
